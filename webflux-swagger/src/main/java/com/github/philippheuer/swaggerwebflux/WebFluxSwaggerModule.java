a6c5d44680b676c53379ebc8ab1352eb6a1ed6dd
Philipp Heuer <git@philippheuer.me>
2019-01-31 23:28:48 +0100

    feature: configure using swagger.properties

--- webflux-swagger/src/main/java/com/github/philippheuer/swaggerwebflux/WebFluxSwaggerModule.java
+++ webflux-swagger/src/main/java/com/github/philippheuer/swaggerwebflux/WebFluxSwaggerModule.java
@@ -2,7 +2,12 @@ package com.github.philippheuer.swaggerwebflux;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+/**
+ * Swagger WebFlux Module
+ */
 @Configuration
 @ComponentScan("com.github.philippheuer.swaggerwebflux")
+@PropertySource("classpath:swagger.properties")
 public class WebFluxSwaggerModule {}
--- webflux-swagger/src/test/java/com/github/philippheuer/swaggerwebflux/Application.java
+++ webflux-swagger/src/test/java/com/github/philippheuer/swaggerwebflux/Application.java
@@ -2,10 +2,12 @@ package com.github.philippheuer.swaggerwebflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableWebFlux
 @SpringBootApplication
+@Import(WebFluxSwaggerModule.class)
 public class Application {
 
     public static void main(String[] args) {
