0c9daaa452e575b81d7d34f24164536d614d8232
Philipp Heuer <git@philippheuer.me>
2019-04-20 01:53:22 +0200

    feature: add auto configuration class for webflux tracing module

---
+++ webflux-tracing/src/main/java/com/github/philippheuer/webflux/tracing/CustomWebFluxTracingAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.github.philippheuer.webflux.tracing;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.github.philippheuer.webflux.tracing")
+public class CustomWebFluxTracingAutoConfiguration {
+
+}
