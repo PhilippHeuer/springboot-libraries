d6ee557965fbaf8dd0922021c1cffbc1e8c47264
Philipp Heuer <git@philippheuer.me>
2019-04-20 01:22:50 +0200

    feature: add webflux-tracing & webflux-tracing-jaeger

---
+++ webflux-tracing/src/main/java/com/github/philippheuer/webflux/tracing/config/TracingConfiguration.java
@@ -0,0 +1,32 @@
+package com.github.philippheuer.webflux.tracing.config;
+
+import io.opentracing.Tracer;
+import io.opentracing.contrib.spring.web.webfilter.TracingWebFilter;
+import io.opentracing.contrib.spring.web.webfilter.WebFluxSpanDecorator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.regex.Pattern;
+
+/**
+ * Configure the WebFlux Tracing Web Filter
+ */
+@Configuration
+@ConditionalOnProperty(name = "opentracing.enabled", havingValue = "true", matchIfMissing = true)
+class TracingConfiguration {
+
+    @Bean
+    public TracingWebFilter tracingWebFilter(Tracer tracer) {
+        return new TracingWebFilter(
+            tracer,
+            Integer.MIN_VALUE,                // Order
+            Pattern.compile("/actuator/*"),   // Skip pattern
+            Collections.singletonList("/**"), // URL patterns
+            Arrays.asList(new WebFluxSpanDecorator.StandardTags(), new WebFluxSpanDecorator.WebFluxTags())
+        );
+    }
+
+}
---
+++ webflux-tracing/src/main/java/com/github/philippheuer/webflux/tracing/webfilter/ResponseTraceIdInjector.java
@@ -0,0 +1,53 @@
+package com.github.philippheuer.webflux.tracing.webfilter;
+
+import io.opentracing.Tracer;
+import io.opentracing.propagation.Format;
+import io.opentracing.propagation.TextMapInjectAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Response TraceId Injector
+ * <p>
+ * Injects the trace id into the the response headers (based on the used codec)
+ */
+@Component
+@Order(Integer.MAX_VALUE)
+public class ResponseTraceIdInjector implements WebFilter {
+
+    @Autowired
+    Tracer tracer;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        // inject tracing information into the response headers
+        Map<String, String> tracingHeaders = new HashMap<>();
+        tracer.inject(tracer.activeSpan().context(), Format.Builtin.HTTP_HEADERS, new TextMapInjectAdapter(tracingHeaders));
+        exchange.getResponse().getHeaders().addAll(convertMapToMultiValueMap(tracingHeaders));
+
+        return chain.filter(exchange);
+    }
+
+    private Map<String, String> convertHeadersToMap(HttpHeaders httpHeaders) {
+        Map<String, String> map = new HashMap<>();
+        httpHeaders.entrySet().forEach(entry -> map.put(entry.getKey(), entry.getValue().get(0)));
+        return map;
+    }
+
+    private MultiValueMap convertMapToMultiValueMap(Map<String, String> map) {
+        MultiValueMap multiValueMap = new LinkedMultiValueMap<String, String>();
+        multiValueMap.setAll(map);
+        return multiValueMap;
+    }
+}
