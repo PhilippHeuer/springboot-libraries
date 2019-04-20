bc7336ac400aa664aeb7629afe14b8c8c45e18fd
Philipp Heuer <git@philippheuer.me>
2019-04-21 00:51:15 +0200

    feature: add support for opentracing api extensions

--- webflux-tracing/src/main/java/com/github/philippheuer/webflux/tracing/webfilter/ResponseTraceIdInjector.java
+++ webflux-tracing/src/main/java/com/github/philippheuer/webflux/tracing/webfilter/ResponseTraceIdInjector.java
@@ -2,7 +2,7 @@ package com.github.philippheuer.webflux.tracing.webfilter;
 
 import io.opentracing.Tracer;
 import io.opentracing.propagation.Format;
-import io.opentracing.propagation.TextMapInjectAdapter;
+import io.opentracing.propagation.TextMapAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
@@ -33,7 +33,7 @@ public class ResponseTraceIdInjector implements WebFilter {
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         // inject tracing information into the response headers
         Map<String, String> tracingHeaders = new HashMap<>();
-        tracer.inject(tracer.activeSpan().context(), Format.Builtin.HTTP_HEADERS, new TextMapInjectAdapter(tracingHeaders));
+        tracer.inject(tracer.activeSpan().context(), Format.Builtin.HTTP_HEADERS, new TextMapAdapter(tracingHeaders));
         exchange.getResponse().getHeaders().addAll(convertMapToMultiValueMap(tracingHeaders));
 
         return chain.filter(exchange);
